0e6792dd03d017544303cf770a0bf71b44d39fb9
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2026-06-20 13:06:42 +0300

    Create config/passport.js

---
+++ config/passport.js
@@ -0,0 +1,58 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User');
+
+module.exports = function(passport) {
+  // Serialize user
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  // Deserialize user
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  });
+
+  // Local Strategy
+  passport.use(new LocalStrategy(
+    { usernameField: 'username' },
+    async (username, password, done) => {
+      try {
+        // Find user by username or email
+        const user = await User.findOne({
+          $or: [
+            { username: username.toLowerCase() },
+            { email: username.toLowerCase() }
+          ]
+        });
+
+        if (!user) {
+          return done(null, false, { 
+            message: '❌ Username au email haipatikani.' 
+          });
+        }
+
+        // Check password
+        const isValid = await user.comparePassword(password);
+        if (!isValid) {
+          return done(null, false, { 
+            message: '❌ Password sio sahihi.' 
+          });
+        }
+
+        // Update last login
+        user.lastLogin = new Date();
+        await user.save();
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  ));
+};
